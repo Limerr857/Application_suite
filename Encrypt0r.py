3f3af0b28d370550610b9661fcc46e880f18e093
Limerr857 <georgelgeback@gmail.com>
2019-09-27 12:08:53 +0200

    Added a new level of encryption

--- Encrypt0r.py
+++ Encrypt0r.py
@@ -13,7 +13,7 @@ z3@flä$nrhqök&4u^0(6=8bisy71-g,2~;':9å€x	"""  # Used for second, third and 
 textheight = 8
 lvl3bg = 50
 lvl3sm = 20
-
+lvl5_active = False
 
 def strt():  # Start of the program, screen where you can choose between encrypt and decrypt.
     # Clears the entire window without deleting root
@@ -34,11 +34,13 @@ def encrypt():
     btn9 = Button(window, text = "Level 2", command = encrypt2)
     btn10 = Button(window, text = "Level 3", command = encrypt3)
     btn11 = Button(window, text = "Level 4", command = encrypt4)
+    btn12 = Button(window, text = "Level 5", command = encrypt5)
     lbl6.pack(side = TOP)
     btn8.pack(fill = X)
     btn9.pack(fill = X)
     btn10.pack(fill = X)
     btn11.pack(fill = X)
+    btn12.pack(fill = X)
 
 def decrypt():
     for widget in window.winfo_children():
@@ -48,14 +50,16 @@ def decrypt():
     btn9 = Button(window, text = "Level 2", command = decrypt2)
     btn10 = Button(window, text = "Level 3", command = decrypt3)
     btn11 = Button(window, text = "Level 4", command = decrypt4)
+    btn12 = Button(window, text = "Level 5", command = decrypt5)
     lbl6.pack(side = TOP)
     btn8.pack(fill = X)  # Fill is used for A S T H E T I C S
     btn9.pack(fill = X)
     btn10.pack(fill = X)
     btn11.pack(fill = X)
+    btn12.pack(fill = X)
 
 
-  # Level 1
+# Level 1
 def encrypt1(): # Easiest form of encrypting, uses something very similar to the ceasar chipher
     for widget in window.winfo_children():
         widget.destroy()
@@ -68,9 +72,13 @@ def encrypt1(): # Easiest form of encrypting, uses something very similar to the
     txt2.pack()
     
 def encrypt11():  # Multiple def's used because of lazyness
-    temp2 = txt2.get('1.0', 'end-1c')  # Gets everything from the inptted text
-    for widget in window.winfo_children():
-        widget.destroy()
+    global message
+    global txt2
+    global temp2
+    if lvl5_active == False:
+        temp2 = txt2.get('1.0', 'end-1c')  # Gets everything from the inputted text
+        for widget in window.winfo_children():
+            widget.destroy()
     temp2 = str.lower(temp2)
     message = list(temp2)
     msglen = len(message)
@@ -90,15 +98,16 @@ def encrypt11():  # Multiple def's used because of lazyness
         message[x] = temp1
         x += 1
     message = ''.join(message)
-    txt1 = Text(window, height = textheight) 
-    txt1.insert(1.0, message)
-    lbl3 = Label(window, text = "Here is your encrypted message:")
-    btn5 = Button(window, text = "Restart Encrypt0r", command = strt)
-    lbl3.pack(side = TOP)
-    txt1.pack()
-    btn5.pack(side = BOTTOM)
-    txt1.configure(bg=window.cget('bg'), relief=FLAT)  # These two might be useless, but I can't be bothered to check
-    txt1.configure(state="disabled")      
+    if lvl5_active == False:
+        txt1 = Text(window, height = textheight) 
+        txt1.insert(1.0, message)
+        lbl3 = Label(window, text = "Here is your encrypted message:")
+        btn5 = Button(window, text = "Restart Encrypt0r", command = strt)
+        lbl3.pack(side = TOP)
+        txt1.pack()
+        btn5.pack(side = BOTTOM)
+        txt1.configure(bg=window.cget('bg'), relief=FLAT)   
+        txt1.configure(state="disabled")      
 
 
 def decrypt1(): # Easiest form of decrypting, uses something very similar to the ceasar chipher
@@ -113,10 +122,13 @@ def decrypt1(): # Easiest form of decrypting, uses something very similar to the
     txt2.pack()
 
 def decrypt11():
-    global txt2
-    temp2 = txt2.get('1.0', 'end-1c')  # Gets everything from the inptted text
-    for widget in window.winfo_children():
-        widget.destroy()
+    global temp2
+    global message
+    if lvl5_active == False:
+        global txt2
+        temp2 = txt2.get('1.0', 'end-1c')  # Gets everything from the inptted text
+        for widget in window.winfo_children():
+            widget.destroy()
     message = list(temp2)
     msglen = len(message)
     x = 0
@@ -135,18 +147,19 @@ def decrypt11():
         message[x] = temp1
         x += 1
     message = ''.join(message)
-    txt2 = Text(window, height = textheight) 
-    txt2.insert(1.0, message)
-    lbl5 = Label(window, text = "Here is your decrypted message:")
-    btn7 = Button(window, text = "Restart Encrypt0r", command = strt)
-    lbl5.pack(side = TOP)
-    txt2.pack()
-    btn7.pack(side = BOTTOM)
-    txt2.configure(bg=window.cget('bg'), relief=FLAT)  # These two might be useless, but I can't be bothered to check
-    txt2.configure(state="disabled") 
+    if lvl5_active == False:
+        txt2 = Text(window, height = textheight) 
+        txt2.insert(1.0, message)
+        lbl5 = Label(window, text = "Here is your decrypted message:")
+        btn7 = Button(window, text = "Restart Encrypt0r", command = strt)
+        lbl5.pack(side = TOP)
+        txt2.pack()
+        btn7.pack(side = BOTTOM)
+        txt2.configure(bg=window.cget('bg'), relief=FLAT)   
+        txt2.configure(state="disabled") 
 
 
-  # Level 2    
+# Level 2    
 def encrypt2():  # Second to easiest form of encryption, same as level 1 but with shuffled letters and symbols
     for widget in window.winfo_children():
         widget.destroy()
@@ -159,9 +172,12 @@ def encrypt2():  # Second to easiest form of encryption, same as level 1 but wit
     txt2.pack()
 
 def encrypt22():
-    temp2 = txt2.get('1.0', 'end-1c')  # Gets everything from the inputted text
-    for widget in window.winfo_children():
-        widget.destroy()
+    global message
+    global temp2
+    if lvl5_active == False:
+        temp2 = txt2.get('1.0', 'end-1c')  # Gets everything from the inputted text
+        for widget in window.winfo_children():
+            widget.destroy()
     temp2 = str.lower(temp2)
     message = list(temp2)
     msglen = len(message)
@@ -181,15 +197,16 @@ def encrypt22():
         message[x] = temp1
         x += 1
     message = ''.join(message)
-    txt1 = Text(window, height = textheight) 
-    txt1.insert(1.0, message)
-    lbl3 = Label(window, text = "Here is your encrypted message:")
-    btn5 = Button(window, text = "Restart Encrypt0r", command = strt)
-    lbl3.pack(side = TOP)
-    txt1.pack()
-    btn5.pack(side = BOTTOM)
-    txt1.configure(bg=window.cget('bg'), relief=FLAT)  # These two might be useless, but I can't be bothered to check
-    txt1.configure(state="disabled")      
+    if lvl5_active == False:
+        txt1 = Text(window, height = textheight) 
+        txt1.insert(1.0, message)
+        lbl3 = Label(window, text = "Here is your encrypted message:")
+        btn5 = Button(window, text = "Restart Encrypt0r", command = strt)
+        lbl3.pack(side = TOP)
+        txt1.pack()
+        btn5.pack(side = BOTTOM)
+        txt1.configure(bg=window.cget('bg'), relief=FLAT)   
+        txt1.configure(state="disabled")      
 
 
 def decrypt2():  # Second to easiest form of decryption, same as level 1 but with shuffled letters and symbols
@@ -204,10 +221,13 @@ def decrypt2():  # Second to easiest form of decryption, same as level 1 but wit
     txt2.pack()
 
 def decrypt22():
-    global txt2
-    temp2 = txt2.get("1.0",'end-1c')  # Gets everything from the inptted text
-    for widget in window.winfo_children():
-        widget.destroy()
+    global temp2
+    global message
+    if lvl5_active == False:
+        global txt2
+        temp2 = txt2.get("1.0",'end-1c')  # Gets everything from the inptted text
+        for widget in window.winfo_children():
+            widget.destroy()
     message = list(temp2)
     msglen = len(message)
     x = 0
@@ -226,18 +246,19 @@ def decrypt22():
         message[x] = temp1
         x += 1
     message = ''.join(message)
-    txt2 = Text(window, height = textheight) 
-    txt2.insert(1.0, message)
-    lbl5 = Label(window, text = "Here is your decrypted message:")
-    btn7 = Button(window, text = "Restart Encrypt0r", command = strt)
-    lbl5.pack(side = TOP)
-    txt2.pack()
-    btn7.pack(side = BOTTOM)
-    txt2.configure(bg=window.cget('bg'), relief=FLAT)  # These two might be useless, but I can't be bothered to check
-    txt2.configure(state="disabled") 
+    if lvl5_active == False:
+        txt2 = Text(window, height = textheight) 
+        txt2.insert(1.0, message)
+        lbl5 = Label(window, text = "Here is your decrypted message:")
+        btn7 = Button(window, text = "Restart Encrypt0r", command = strt)
+        lbl5.pack(side = TOP)
+        txt2.pack()
+        btn7.pack(side = BOTTOM)
+        txt2.configure(bg=window.cget('bg'), relief=FLAT)   
+        txt2.configure(state="disabled") 
 
 
-  # Level 3
+# Level 3
 def encrypt3():  # Third most insecure, this time a key is also used, wich eliminates looking through the code to find the letters.
     for widget in window.winfo_children():
         widget.destroy()
@@ -264,11 +285,27 @@ def encrypt31():
     txt2.pack()
 
 def encrypt33():
-    global txt2
-    key = txt2.get("1.0",'end-1c')  # Gets everything from the inputted text
-    for widget in window.winfo_children():
-        widget.destroy()
+    global message
     global temp2
+    global key
+    if lvl5_active == False:
+        global txt2
+        key = txt2.get("1.0",'end-1c')  # Gets everything from the inputted text
+        for widget in window.winfo_children():
+            widget.destroy()
+        global temp2
+    try:
+        key = int(key)
+    except:
+        key = list(key)
+        x = 0
+        global notalphabet
+        for i in key:
+            temp1 = notalphabet.find(i)
+            key[x] = temp1
+            x += 1
+        key = '+'.join(str(e) for e in key)
+        key = eval(key)
     temp2 = str.lower(temp2)
     message = list(temp2)
     x = 0
@@ -276,20 +313,21 @@ def encrypt33():
         i = notalphabet.find(i)
         # The core of the calculation
         random.seed(key)
-        i = i * random.randint(lvl3sm, lvl3bg)
+        i = i * random.randint(20, 50)
         #
         message[x] = i
         x += 1
     message = '    '.join(str(e) for e in message)
-    txt2 = Text(window, height = 8, wrap = WORD) 
-    txt2.insert(1.0, message)
-    lbl5 = Label(window, text = "Here is your encrypted message:")
-    btn7 = Button(window, text = "Restart Encrypt0r", command = strt)
-    lbl5.pack(side = TOP)
-    txt2.pack()
-    btn7.pack(side = BOTTOM)
-    txt2.configure(bg=window.cget('bg'), relief=FLAT)  # These two might be useless, but I can't be bothered to check
-    txt2.configure(state="disabled") 
+    if lvl5_active == False:
+        txt2 = Text(window, height = 8, wrap = WORD) 
+        txt2.insert(1.0, message)
+        lbl5 = Label(window, text = "Here is your encrypted message:")
+        btn7 = Button(window, text = "Restart Encrypt0r", command = strt)
+        lbl5.pack(side = TOP)
+        txt2.pack()
+        btn7.pack(side = BOTTOM)
+        txt2.configure(bg=window.cget('bg'), relief=FLAT)   
+        txt2.configure(state="disabled") 
 
 
 def decrypt3():
@@ -318,35 +356,52 @@ def decrypt31():
     txt2.pack()
 
 def decrypt33():
-    global txt2
-    key = txt2.get("1.0",'end-1c')  # Gets everything from the inputted text
-    for widget in window.winfo_children():
-        widget.destroy()
+    global message
+    global temp2
+    global key
+    if lvl5_active == False:
+        global txt2
+        key = txt2.get("1.0",'end-1c')  # Gets everything from the inputted text
+        for widget in window.winfo_children():
+            widget.destroy()
+    try:
+        key = int(key)
+    except:
+        key = list(key)
+        x = 0
+        global notalphabet
+        for i in key:
+            temp1 = notalphabet.find(i)
+            key[x] = temp1
+            x += 1
+        key = '+'.join(str(e) for e in key)
+        key = eval(key)
     message = temp2.split()
     x = 0
     for i in message:
         i = int(i)
         # The core of the calculation
         random.seed(key)
-        i = i / random.randint(lvl3sm, lvl3bg)
+        i = i / random.randint(20, 50)
         #
         i = int(i)
         i = notalphabet[i]
         message[x] = i
         x += 1
     message = ''.join(str(e) for e in message)
-    txt2 = Text(window, height = 8, wrap = WORD) 
-    txt2.insert(1.0, message)
-    lbl5 = Label(window, text = "Here is your decrypted message:")
-    btn7 = Button(window, text = "Restart Encrypt0r", command = strt)
-    lbl5.pack(side = TOP)
-    txt2.pack()
-    btn7.pack(side = BOTTOM)
-    txt2.configure(bg=window.cget('bg'), relief=FLAT)  # These two might be useless, but I can't be bothered to check
-    txt2.configure(state="disabled") 
+    if lvl5_active == False:
+        txt2 = Text(window, height = 8, wrap = WORD) 
+        txt2.insert(1.0, message)
+        lbl5 = Label(window, text = "Here is your decrypted message:")
+        btn7 = Button(window, text = "Restart Encrypt0r", command = strt)
+        lbl5.pack(side = TOP)
+        txt2.pack()
+        btn7.pack(side = BOTTOM)
+        txt2.configure(bg=window.cget('bg'), relief=FLAT)   
+        txt2.configure(state="disabled") 
 
 
-  # Level 4
+# Level 4
 def encrypt4():  # Fourth most insecure, this time a key is used to generate a random sequence to raise the numbers by.
     for widget in window.winfo_children():
         widget.destroy()
@@ -373,10 +428,13 @@ def encrypt41():
     txt2.pack()
 
 def encrypt44():
-    global txt2
-    key = txt2.get("1.0",'end-1c')  # Gets everything from the inputted text
-    for widget in window.winfo_children():
-        widget.destroy()
+    global message
+    global key
+    if lvl5_active == False:
+        global txt2
+        key = txt2.get("1.0",'end-1c')  # Gets everything from the inputted text
+        for widget in window.winfo_children():
+            widget.destroy()
     try:
         key = int(key)
     except:
@@ -416,16 +474,17 @@ def encrypt44():
         message[x] = i
         x += 1
     message = '    '.join(str(e) for e in message)
-    txt2 = Text(window, height = 8, wrap = WORD) 
-    txt2.insert(1.0, message)
-    lbl5 = Label(window, text = "Here is your encrypted message:")
-    btn7 = Button(window, text = "Restart Encrypt0r", command = strt)
-    scr1 = Scrollbar(window, command=txt2.yview)
-    txt2['yscrollcommand'] = scr1.set
-    scr1.grid(row = 2, column = 1, sticky="ns")
-    lbl5.grid(row = 1)
-    txt2.grid(row = 2)
-    btn7.grid(row = 3)
+    if lvl5_active == False:
+        txt2 = Text(window, height = 8, wrap = WORD) 
+        txt2.insert(1.0, message)
+        lbl5 = Label(window, text = "Here is your encrypted message:")
+        btn7 = Button(window, text = "Restart Encrypt0r", command = strt)
+        scr1 = Scrollbar(window, command=txt2.yview)
+        txt2['yscrollcommand'] = scr1.set
+        scr1.grid(row = 2, column = 1, sticky="ns")
+        lbl5.grid(row = 1)
+        txt2.grid(row = 2)
+        btn7.grid(row = 3)
 
 
 def decrypt4():  # Fourth most insecure, this time a key is used to generate a random sequence to raise the numbers by.
@@ -454,11 +513,15 @@ def decrypt41():
     txt2.pack()
 
 def decrypt44():
-    global txt2
+    global message
     global temp2
-    key = txt2.get("1.0",'end-1c')  # Gets everything from the inputted text
-    for widget in window.winfo_children():
-        widget.destroy()
+    global key
+    if lvl5_active == False:
+        global txt2
+        global temp2
+        key = txt2.get("1.0",'end-1c')  # Gets everything from the inputted text
+        for widget in window.winfo_children():
+            widget.destroy()
     try:
         key = int(key)
     except:
@@ -500,6 +563,114 @@ def decrypt44():
         x += 1
     notalphabet = ''.join(str(e) for e in notalphabet)
     message = ''.join(str(e) for e in message)
+    if lvl5_active == False:
+        txt2 = Text(window, height = 8, wrap = WORD) 
+        txt2.insert(1.0, message)
+        lbl5 = Label(window, text = "Here is your decrypted message:")
+        btn7 = Button(window, text = "Restart Encrypt0r", command = strt)
+        lbl5.pack(side = TOP)
+        txt2.pack()
+        btn7.pack(side = BOTTOM)
+        txt2.configure(bg=window.cget('bg'), relief=FLAT)   
+        txt2.configure(state="disabled") 
+
+
+
+def encrypt5():  # Fifth most insecure, uses every lower level of encryption combined.
+    for widget in window.winfo_children():
+        widget.destroy()
+    global txt2
+    txt2 = Text(window, height = textheight)  
+    btn4 = Button(window, text = "Encrypt", command = encrypt51)
+    lbl2 = Label(window, text = 'Write the message you want to encrypt and press "Encrypt"')
+    lbl2.pack(side = TOP)
+    btn4.pack(side = BOTTOM)
+    txt2.pack()
+
+def encrypt51():
+    global txt2
+    global temp2
+    temp2 = txt2.get("1.0",'end-1c')  # Gets everything from the inputted text
+    for widget in window.winfo_children():
+        widget.destroy()
+    global key
+    txt2 = Text(window, height = textheight)  
+    btn4 = Button(window, text = "Use key", command = encrypt55)
+    lbl2 = Label(window, text = 'Write the key you want to use, the larger the key the better the encryption.\nYou will need to securely send this key to your recipient.\nYou can use both numbers, letters and special characters.')
+    lbl2.pack(side = TOP)
+    btn4.pack(side = BOTTOM)
+    txt2.pack()
+
+def encrypt55():
+    global txt2
+    global key
+    global lvl5_active
+    global temp2
+    lvl5_active = True
+    key = txt2.get("1.0",'end-1c')  # Gets everything from the inputted text
+    for widget in window.winfo_children():
+        widget.destroy()
+    encrypt22()
+    temp2 = message
+    encrypt33()
+    temp2 = message
+    encrypt44()
+    temp2 = message
+
+
+    txt2 = Text(window, height = 8, wrap = WORD) 
+    txt2.insert(1.0, message)
+    lbl5 = Label(window, text = "Here is your encrypted message:")
+    btn7 = Button(window, text = "Restart Encrypt0r", command = strt)
+    scr1 = Scrollbar(window, command=txt2.yview)
+    txt2['yscrollcommand'] = scr1.set
+    scr1.grid(row = 2, column = 1, sticky="ns")
+    lbl5.grid(row = 1)
+    txt2.grid(row = 2)
+    btn7.grid(row = 3)
+
+
+def decrypt5():  # Fifth most insecure, uses every lower level of encryption combined.
+    for widget in window.winfo_children():
+        widget.destroy()
+    global txt2
+    txt2 = Text(window, height = textheight)  
+    btn4 = Button(window, text = "Decrypt", command = decrypt51)
+    lbl2 = Label(window, text = 'Write the message you want to encrypt and press "Decrypt"')
+    lbl2.pack(side = TOP)
+    btn4.pack(side = BOTTOM)
+    txt2.pack()
+
+def decrypt51():
+    global txt2
+    global temp2
+    temp2 = txt2.get("1.0",'end-1c')  # Gets everything from the inputted text
+    for widget in window.winfo_children():
+        widget.destroy()
+    global key
+    txt2 = Text(window, height = textheight)  
+    btn4 = Button(window, text = "Use key", command = decrypt55)
+    lbl2 = Label(window, text = 'Type or paste the key that the sender has sent you securely.')
+    lbl2.pack(side = TOP)
+    btn4.pack(side = BOTTOM)
+    txt2.pack()
+
+def decrypt55():
+    global txt2
+    global key
+    global lvl5_active
+    global temp2
+    lvl5_active = True
+    key = txt2.get("1.0",'end-1c')  # Gets everything from the inputted text
+    for widget in window.winfo_children():
+        widget.destroy()
+    decrypt44()
+    temp2 = message
+    decrypt33()
+    temp2 = message
+    decrypt22()
+
+
     txt2 = Text(window, height = 8, wrap = WORD) 
     txt2.insert(1.0, message)
     lbl5 = Label(window, text = "Here is your decrypted message:")
@@ -507,9 +678,8 @@ def decrypt44():
     lbl5.pack(side = TOP)
     txt2.pack()
     btn7.pack(side = BOTTOM)
-    txt2.configure(bg=window.cget('bg'), relief=FLAT)  # These two might be useless, but I can't be bothered to check
+    txt2.configure(bg=window.cget('bg'), relief=FLAT)   
     txt2.configure(state="disabled") 
-
 
 global window
 window = Tk()
