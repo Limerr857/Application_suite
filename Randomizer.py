ae4552caddfcf5150d8cf0189fe49d73c27aa523
Limerr857 <georgelgeback@gmail.com>
2019-10-01 19:18:36 +0200

    Fixed some bugs and updated randomizer

--- Randomizer.py
+++ Randomizer.py
@@ -4,19 +4,21 @@ from tkinter.ttk import *
 import random
 
 temp = ""
-alphabet = "abcdefghijklmnopqrstuvwxyz"
-symbols = "abcdefghijklmnopqrstuvwxyzåäö.,/_-+=():'*?!1234567890&$€#@^~;"
+alphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+symbols = "abcdefghijklmnopqrstuvwxyzåäöABCDEFGHIJKLMNOPQRSTUVWXYZÅÄÖ.,/_-+=():'*?!1234567890&$€#@^~;"
 alen = len(alphabet)
 sylen = len(symbols)
 
 def begin():
+    global temp
+    temp = ""
     for widget in root.winfo_children():
         widget.destroy()
     lbl1 = Label(root, text = "What do you want to do?").pack(side = TOP)
     btn2 = Button(root, text = "Random numbers!", command = Num).pack(fill=X)
     btn3 = Button(root, text = "Random letters!", command = Let).pack(fill=X)
-    #btn4 = Button(root, text = "Random symbols!", command = Robot).pack(fill=X)
-    #btn5 = Button(root, text = "Random numbers and symbols!", command = Country).pack(fill=X)
+    btn4 = Button(root, text = "Random symbols!", command = Sym).pack(fill=X)
+    btn5 = Button(root, text = "Random numbers and symbols!", command = NumSym).pack(fill=X)
 
 def Num():
     global ent1
@@ -25,9 +27,11 @@ def Num():
     txt1 = Text(root, height = 8, wrap = WORD) 
     txt1.insert(1.0, temp)
     btn1 = Button(root, text="Give me those numbers!", command=Num2)
+    btn2 = Button(root, text="Go back", command=begin)
     lbl2 = Label(root, text="length:")
     ent1 = Entry(root)
     txt1.pack(side=TOP)
+    btn2.pack(side=BOTTOM)
     btn1.pack(side=BOTTOM)
     lbl2.pack()
     ent1.pack()
@@ -37,13 +41,15 @@ def Num2():
     try:
         temp2 = int(temp2)
         temp2 -= 1
+        failed = False
     except:
         temp = "Only integers"
-        Num()
-    if temp2 > 10000:
+        failed = True
+    if failed == False:
+        if temp2 > 10000:
             temp = "Only numbers below 10000"
-            Num()
-    temp = random.randint((10 ** temp2), (10**(temp2+1))-1)
+        else:
+            temp = random.randint((10 ** temp2), (10**(temp2+1))-1)
     Num()
 
 def Let():
@@ -53,9 +59,11 @@ def Let():
     txt1 = Text(root, height = 8, wrap = WORD) 
     txt1.insert(1.0, temp)
     btn1 = Button(root, text="Give me those letters!", command=Let2)
+    btn2 = Button(root, text="Go back", command=begin)
     lbl2 = Label(root, text="length:")
     ent1 = Entry(root)
     txt1.pack(side=TOP)
+    btn2.pack(side=BOTTOM)
     btn1.pack(side=BOTTOM)
     lbl2.pack()
     ent1.pack()
@@ -64,20 +72,99 @@ def Let2():
     temp2 = ent1.get()
     try:
         temp2 = int(temp2)
-        temp2 -= 1
+        failed = False
     except:
         temp = "Only integers"
-        Let()
-    if temp2 > 10000:
+        failed = True
+    if failed == False:
+        if temp2 > 10000:
             temp = "Only numbers below 10000"
-            Let()
-    temp = []
-    for i in range(temp2):
-        temp3 = random.randint(0, alen)
-        temp3 = alphabet[temp3-1]
-        temp.append(temp3)
-    temp = "".join(temp)
+        else:
+            temp = []
+            for i in range(temp2):
+                temp3 = random.randint(0, alen)
+                temp3 = alphabet[temp3-1]
+                temp.append(temp3)
+            temp = "".join(temp)
     Let()
+
+def Sym():
+    global ent1
+    for widget in root.winfo_children():
+        widget.destroy()
+    txt1 = Text(root, height = 8, wrap = WORD) 
+    txt1.insert(1.0, temp)
+    btn1 = Button(root, text="Give me those symbols!", command=Sym2)
+    btn2 = Button(root, text="Go back", command=begin)
+    lbl2 = Label(root, text="length:")
+    ent1 = Entry(root)
+    txt1.pack(side=TOP)
+    btn2.pack(side=BOTTOM)
+    btn1.pack(side=BOTTOM)
+    lbl2.pack()
+    ent1.pack()
+def Sym2():
+    global temp
+    temp2 = ent1.get()
+    try:
+        temp2 = int(temp2)
+        failed = False
+    except:
+        temp = "Only integers"
+        failed = True
+    if failed == False:
+        if temp2 > 10000:
+            temp = "Only numbers below 10000"
+        else:
+            temp = []
+            for i in range(temp2):
+                temp3 = random.randint(0, sylen)
+                temp3 = symbols[temp3-1]
+                temp.append(temp3)
+            temp = "".join(temp)
+    Sym()
+
+def NumSym():
+    global ent1
+    for widget in root.winfo_children():
+        widget.destroy()
+    txt1 = Text(root, height = 8, wrap = WORD) 
+    txt1.insert(1.0, temp)
+    btn1 = Button(root, text="Give me those numbers and symbols!", command=NumSym2)
+    btn2 = Button(root, text="Go back", command=begin)
+    lbl2 = Label(root, text="length:")
+    ent1 = Entry(root)
+    txt1.pack(side=TOP)
+    btn2.pack(side=BOTTOM)
+    btn1.pack(side=BOTTOM)
+    lbl2.pack()
+    ent1.pack()
+def NumSym2():
+    global temp
+    temp2 = ent1.get()
+    try:
+        temp2 = int(temp2)
+        failed = False
+    except:
+        temp = "Only integers"
+        failed = True
+    if failed == False:
+        if temp2 > 10000:
+            temp = "Only numbers below 10000"
+        else:
+            temp = []
+            for i in range(temp2):
+                temp3 = random.randint(0, 1)
+                if temp3 == 0:
+                    temp3 = random.randint(0, sylen)
+                    temp3 = symbols[temp3-1]
+                    temp.append(temp3)
+                else:
+                    temp3 = str(random.randint(0, 9))
+                    temp.append(temp3)
+            temp = "".join(temp)
+    NumSym()
+
 
 root = Toplevel()
 root.geometry("400x300")
